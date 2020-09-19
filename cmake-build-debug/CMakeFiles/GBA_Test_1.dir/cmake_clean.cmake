4dbe392e621e7a74dc28f8591683ec2f873ccf6c
Connor Bell <cjbell630@users.noreply.github.com>
2020-09-18 23:18:33 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/GBA_Test_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GBA_Test_1.dir/main.c.obj"
+  "GBA_Test_1.exe"
+  "GBA_Test_1.exe.manifest"
+  "GBA_Test_1.pdb"
+  "libGBA_Test_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GBA_Test_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GBA_Test_1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GBA_Test_1.
+# This may be replaced when dependencies are built.
